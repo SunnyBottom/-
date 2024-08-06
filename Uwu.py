b9907e0adc957994645b161356a80814e5536623
SunnyBottom <kristinaeme@gmail.com>
2024-08-06 13:56:48 +0500

    Add files via upload

---
+++ Uwu.py
@@ -0,0 +1,11 @@
+# Name: John
+# Age: 25
+# New Age: 26
+# Is Student: True
+name_a = ' Кристина'
+print('Имя:' + name_a)
+age = 17
+print('Старый возраст:', age)
+print('Новый возраст:', age + 1)
+is_student = True
+print('Студент:', is_student)
